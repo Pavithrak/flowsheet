3489be89da6c7c78f5cd6ef117a6d49d04d3ea40
Khaarthi <khaarthi>
2010-10-27 18:31:04 +0530

    [#15] Load observations in reverse chronological order --Sairam/Khaarthigha

--- omod/src/main/resources/flowsheet.js
+++ omod/src/main/resources/flowsheet.js
@@ -1,18 +1,21 @@
 var Flowsheet = function() {
 
-	this.render = function(flowsheetData,tableId){
-		jQuery("#"+tableId).jqGrid({
-			data: flowsheetData.entries,
-			datatype: "local", 
-			height: 'auto', 
-			rowNum: 100, 
-			rowList: [10,20,30], 
-			colNames:['Date','Name', 'Value'], 
-			colModel:[ {name:'obs_date', width:150}, 
-			           {name:'name', width:290}, 
-			           {name:'value',width:100}], 
-           viewrecords: true, caption: "Observations" });
-		
-	}
-	
+    this.render = function(flowsheetData, tableId) {
+        jQuery("#" + tableId).jqGrid({
+            data: flowsheetData.entries,
+            datatype: "local",
+            height: 'auto',
+            rowNum: 100,
+            rowList: [10,20,30],
+            colNames:['Date','Name', 'Value'],
+            colModel:[
+                {name:'obs_date', width:150, sorttype:'date', formatter:'date', datefmt:'d/m/Y'},
+                {name:'name', width:290},
+                {name:'value',width:100}
+            ],
+            sortname: 'obs_date',
+            viewrecords: true, caption: "Observations" , sortorder: "desc"});
+
+    }
+
 }
