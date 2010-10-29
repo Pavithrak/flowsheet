f998b04811b05377d5327dcfcf64a5330e1b8797
Zabil C.M <zabilcm@gmail.com>
2010-10-29 12:45:29 +0530

    Changed the names of the javascript test files and added more test cases.

---
+++ omod/src/test/resources/scripts/test/flowsheet.spec.js
@@ -0,0 +1,28 @@
+var data = {
+    entries : [
+        {name:"Systolic blood pressure",value:"95 mmHg",dataType:"numeric",classType:"Test", obs_date: "2001-01-12"}
+    ]}
+
+Screw.Unit(function() {
+    // Tests are organized into 'describes' and 'its', following the style of RSpec.
+    describe("flowsheet grid", function() {
+        it("should display the concept name of the observation", function() {
+            var flowsheet = new Flowsheet($("#flowsheet"));
+            flowsheet.render(data);
+            var name = $('#1').find('td:nth-child(2)').html() ;
+            expect(name).to(equal,data.entries[0].name);
+        }),
+        it("should display the concept value of the observation", function() {
+            var flowsheet = new Flowsheet($("#flowsheet"));
+            flowsheet.render(data);
+            var value = $('#1').find('td:nth-child(3)').html() ;
+            expect(value).to(equal,data.entries[0].value);
+        }),
+        it("should display date of the observation", function() {
+            var flowsheet = new Flowsheet($("#flowsheet"));
+            flowsheet.render(data);
+            var date = $('#1').find('td:nth-child(1)').html() ;
+            expect(date).to(equal,data.entries[0].obs_date);
+        })
+    })
+});
