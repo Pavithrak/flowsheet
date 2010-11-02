478df553d5c51a1da10ad7e831354ceccd6f05a4
Khaarthi <khaarthi>
2010-11-02 17:52:06 +0530

    [#4] Date Range Slider - screw unit test case --Sairam/Khaarthigha

--- omod/src/main/webapp/resources/dateRangeSlider.js
+++ omod/src/main/webapp/resources/dateRangeSlider.js
@@ -1,20 +1,20 @@
-var DateRangeSlider = function(flowsheetData){
-    var dateRange = flowsheetData.getAllDates();
+var DateRangeSlider = function(flowsheetData) {
+    var dateRange = flowsheetData.getUniqueAndSortedDates();
 
 
-    this.slider = function(sliderId,dateFilterId) {
-        jQuery("#"+sliderId).slider({
-                    range: true,
-                    min: 0,
-                    max: dateRange.length-1,
-                    values: [0,dateRange.length-1],
-                    slide: function(event, ui) {
-                        jQuery("#"+dateFilterId).val( dateRange[ui.values[0]] + ' - ' + dateRange[ui.values[1]]);
-                    }
-                });
+    this.slider = function(sliderId, dateFilterId) {
+        jQuery("#" + sliderId).slider({
+            range: true,
+            min: 0,
+            max: dateRange.length - 1,
+            values: [0,dateRange.length - 1],
+            slide: function(event, ui) {
+                jQuery("#" + dateFilterId).val(dateRange[ui.values[0]] + ' - ' + dateRange[ui.values[1]]);
+            }
+        });
 
-            jQuery("#"+dateFilterId).val(dateRange[jQuery("#"+sliderId).slider("values", 0)] + ' - ' + dateRange[jQuery("#"+sliderId).slider("values", 1)]);
-        };
+        jQuery("#" + dateFilterId).val(dateRange[jQuery("#" + sliderId).slider("values", 0)] + ' - ' + dateRange[jQuery("#" + sliderId).slider("values", 1)]);
+    };
 
 
 }
