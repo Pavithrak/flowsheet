1452f89f13fc5c4d510ef6f3a47e1c33b0717362
Zabil C.M <zabilcm@gmail.com>
2010-11-03 11:22:33 +0530

    merge date range slider to flowsheet .js to improve load time

--- omod/src/test/resources/scripts/test/flowsheet.spec.js
+++ omod/src/test/resources/scripts/test/flowsheet.spec.js
@@ -48,3 +48,14 @@ Screw.Unit(function() {
         })
     })
 });
+
+Screw.Unit(function() {
+    describe("Date range filter", function() {
+        it("should create a date range slider for the observations", function() {
+            var slider = new DateRangeSlider(new FlowsheetData(SampleFlowsheetData()));
+            var dateFilterId = "dateFilter";
+            slider.slider("slider", dateFilterId);
+            expect(jQuery("#"+dateFilterId).val()).to(equal,"2001-01-12 - 2010-01-12");
+        })
+    })
+});
